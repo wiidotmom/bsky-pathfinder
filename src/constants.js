ea669b1dcbf4597617761ade236f71fbb6a3d2d1
igalaxy <hey@igalaxy.dev>
2024-11-29 02:51:00 -0800

    prod

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://127.0.0.1:5173";
+export const BASE_URL = "https://wii.nekoweb.org/bsky-pathfinder";
 export const CLIENT_ID =
 	"https://wii.nekoweb.org/bsky-pathfinder/oauth/client-metadata.json";
